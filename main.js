e9506a6368d0bb82498591dd964d05097b204e59
mdbellalhosen <mdbellalxpro@gmail.com>
2023-04-16 13:51:04 -0700

    all project added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-const hambarger = document.getElementById("navigation");
+const hambarger = document.getElementById("nav");
 const navbar = document.getElementById("navbar");
 const close = document.getElementById("close");
 
@@ -15,5 +15,24 @@ if(close){
         navbar.classList.remove("active");
     })
 }
+
+
+    var mainImg = document.getElementById("mainImg");
+    var min = document.getElementsByClassName("min");
+
+    min[0].onclick = function(){
+        mainImg.src = min[0].src
+    }
+    min[1].onclick = function(){
+        mainImg.src = min[1].src
+    }
+    min[2].onclick = function(){
+        mainImg.src = min[2].src
+    }
+    min[3].onclick = function(){
+        mainImg.src = min[3].src
+    }
+   
+
 
 
